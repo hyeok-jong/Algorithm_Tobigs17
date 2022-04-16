7e8c8f30eec67ee0911178b7366d5d435e3afc74
HyeokJong <jj770206@naver.com>
2022-04-17 00:31:21 +0900

    Create Q2.py

---
+++ week3/Q2.py
@@ -0,0 +1,18 @@
+nums = input()
+from itertools import permutations
+
+
+def solution(n):
+    a = set()
+    for i in range(len(n)):
+        a |= set(map(int, map("".join, permutations(list(n), i + 1))))
+    a -= set(range(0, 2))
+    if len(a) == 0:
+        return 0
+    for i in range(2, int(max(a) ** 0.5) + 1):
+        if len(a) == 0:
+            return 0
+        a -= set(range(i * 2, max(a) + 1, i))
+    return len(a)
+
+print(solution(nums))
